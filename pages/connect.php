2b3c091dc7ce2a7ba689eeddee70251a1fde8250
Joël <mayerjoelmayer@gmail.com>
2022-06-15 11:14:48 +0200

    Update connect.php

--- pages/connect.php
+++ pages/connect.php
@@ -1,19 +1,16 @@
 <?php
-     // gar-connect.php
-    // maakt een connectie met de database 'garage'
-   // Anjo Eijeriks
    $servername = "Localhost";
-   $dbname = "garage";
+   $dbname = "donkey_travel";
    $username = "root";
    $password = "";
 
-try {
-    $conn = new PDO("mysql:host=$servername;
- dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION);
-    echo " Connect is gelukt !<br>";
+try
+{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo " Connect is gelukt!<br>";
 }
+
 catch (PDOException $e)
 {
     echo " Connectie is MISLUKT: " . $e->getMessage();
